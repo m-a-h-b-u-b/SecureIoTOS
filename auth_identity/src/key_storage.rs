cdf19c16e4e3e7fc6651450ec079452aa4726f63
Md Mahbubur Rahman <mahbubur.r.aaman@gmail.com>
2025-09-08 15:38:03 +0600

    init

--- auth_identity/src/key_storage.rs
+++ auth_identity/src/key_storage.rs
@@ -1,6 +1,8 @@
 //! SecureIoTOS Cryptography Module
 //! SecureIoTOS Authentication & Identity Module
-//! License: Apache 2.0
+//! License : Dual License
+//!           - Apache 2.0 for open-source / personal use
+//!           - Commercial license required for closed-source use
 //! Author: Md Mahbubur Rahman
 //! URL: https://m-a-h-b-u-b.github.io
 //! GitHub: https://github.com/m-a-h-b-u-b/SecureIoTOS
--- auth_identity/src/lib.rs
+++ auth_identity/src/lib.rs
@@ -1,5 +1,7 @@
 //! SecureIoTOS Authentication & Identity Module
-//! License: Apache 2.0
+//! License : Dual License
+//!           - Apache 2.0 for open-source / personal use
+//!           - Commercial license required for closed-source use
 //! Author: Md Mahbubur Rahman
 //! URL: https://m-a-h-b-u-b.github.io
 //! GitHub: https://github.com/m-a-h-b-u-b/SecureIoTOS
