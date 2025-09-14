0c457d051ea1b4781c0b676cba26d72ac7424233
Md Mahbubur Rahman <mahbubur.r.aaman@gmail.com>
2025-09-14 09:50:49 +0600

    Refactor comment for clarity

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,3 +1,12 @@
+//! SecureIoTOS Example: Hello World Module
+//! ---------------------------------------
+//! License : Dual License
+//!   - Apache 2.0 for open-source / personal use
+//!   - Commercial license required for closed-source use
+//! Author  : Md Mahbubur Rahman
+//! URL     : https://m-a-h-b-u-b.github.io
+//! GitHub  : https://github.com/m-a-h-b-u-b/SecureIoTOS
+
 // Example Hello World app
 fn main() {
     println!("Hello IoT World!");
